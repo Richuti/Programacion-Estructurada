2867fff38d2d9d6f74ded65f1e691261895fcbbc
Richuti <richarddiazyanez@gmail.com>
2026-08-21 08:01:37 -0600

    Resolucion de las 3 activades realizadas

---
+++ sesion2/conversion.py
@@ -0,0 +1,6 @@
+horas = int(input("Ingrese las horas: "))
+
+minutos = horas * 60
+segundos = minutos * 60
+
+print(f"Las {horas} horas son equivalentes a {minutos} minutos y {segundos} segundos")
---
+++ sesion2/datos_personales.py
@@ -0,0 +1,5 @@
+nombre = input("Ingrese su nombre: ")
+edad = int(input("Ingrese su edad: "))
+carrera = input("Ingrese su carrera: ")
+
+print(f"Me llamo {nombre}, tengo {edad} años y estudio {carrera}")
